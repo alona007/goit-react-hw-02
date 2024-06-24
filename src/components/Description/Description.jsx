a8cc0048666225a8ca1ed930d48fc08662310c21
alona007 <alona.olyksashenko@gmail.com>
2024-06-24 23:06:32 +0200

    fix-description

---
+++ src/components/Description/Description.jsx
@@ -0,0 +1,14 @@
+import clsx from "clsx";
+import css from "./Description.module.css";
+
+export default function Description() {
+  return (
+    <>
+      <h1 className={clsx(css.title)}>Sip Happens Café</h1>
+      <p className={clsx(css.text)}>
+        Please leave your feedback about our service by selecting one of the
+        options below.
+      </p>
+    </>
+  );
+}
